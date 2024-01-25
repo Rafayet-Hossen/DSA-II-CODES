d3acfe6293f56ba732c1eb3a2a619af61cf0dc7c
Md Rafayet Hossen <hossenmdrafayet@gmail.com>
2024-01-26 01:11:51 +0600

    Update Euclidean Algorithm

--- gcd.cpp
+++ gcd.cpp
@@ -8,9 +8,22 @@ int gcd(int x,int y)
         return x;
     return gcd(y, x%y);
 }
+// this process is called Euclidean algorithm by subtraction.
+int GCD(int a,int b)
+{
+    if(a == 0)
+        return b;
+    if(b == 0)
+        return a;
+    if(a == b)
+        return a;
+    if(a>b)
+        return GCD(a-b,b);
+    return GCD(a,b-a);
+}
 int main(){
     int x,y;
     cin>>x>>y;
-    cout<<gcd(x,y)<<endl;
+    cout<<GCD(x,y)<<endl;
     return 0;
 }
