ae65b39ca7aa36e3a591f3949689507721f4b0b2
Md Rafayet Hossen <hossenmdrafayet@gmail.com>
2024-01-26 21:46:14 +0600

    This is problem A B C

---
+++ RecursionCP/B.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+void printDigit(int n)
+{
+    if(n==0)
+        return;
+    printDigit(n-1);
+    cout<<n<<endl;
+}
+int main(){
+    int n;
+    cin>>n;
+    printDigit(n);
+    return 0;
+}
