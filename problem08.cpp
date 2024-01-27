a01595c9cbf5f4c4dbd07d9a249b5fb27360d7d6
Md Rafayet Hossen <hossenmdrafayet@gmail.com>
2024-01-27 22:49:03 +0600

    This will find the frequency of a value in an array

---
+++ problem08.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int frequency(int arr[],int n,int key)
+{
+
+    if(n==1)
+    {
+        if(arr[0] == key)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    else
+    {
+        int val = arr[n-1];
+        int rest = frequency(arr,n-1,key);
+        if(val == key)
+        {
+            return  1+rest;
+        }
+        else
+        {
+            return rest;
+        }
+    }
+
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<< frequency(arr,n,1)<<endl;
+    return 0;
+}
